240f71018520e1d14388e607791a38d9eefb0f26
ahmety3 <168137871+ahmety3@users.noreply.github.com>
2025-02-06 17:04:18 +0300

    C#

---
+++ ÖDEV4/ÖDEV4/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ÖDEV4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string ad, soyad, yas, meslek, cinsiyet;
+            ad = textBox1.Text;
+            soyad = textBox2.Text;
+            meslek = textBox3.Text;
+            cinsiyet = textBox4.Text;
+            yas = maskedTextBox1.Text;
+            listBox1.Items.Add(ad + " "+soyad+" "+meslek+" "+cinsiyet+" "+yas);
+        }
+    }
+}
---
+++ ÖDEV5_DikdörtgenProgramı/ÖDEV5_DikdörtgenProgramı/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ÖDEV5_DikdörtgenProgramı
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int kk, uk, alan, cevre;
+            kk = 10;
+            uk = 15;
+            alan = kk * uk;
+            cevre = kk * 2 + uk * 2;
+            label3.Text = "alan: " + alan + " çEVRE: " + cevre;
+        }
+    }
+}
